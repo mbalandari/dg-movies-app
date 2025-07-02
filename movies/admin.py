b5e2ebcc6287190dc03a349b55d207acd4d984c6
codingMaz <m.balandari.toroghi@gmail.com>
2025-07-02 12:24:15 +0330

    customize admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Movie
 
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
 
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
