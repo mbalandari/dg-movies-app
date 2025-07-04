f0b6161f0050f615ca115e4410abfbb20724fa6e
codingMaz <m.balandari.toroghi@gmail.com>
2025-07-04 11:43:41 +0330

    add Order model to cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    total = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.user.username
