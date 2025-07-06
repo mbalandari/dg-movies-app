817885d23f41c3928080e5a15b322133aca31213
Maz <61194081+mbalandari@users.noreply.github.com>
2025-07-07 00:35:25 +0330

    Update settings.py

--- project_django/settings.py
+++ project_django/settings.py
@@ -145,5 +145,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "uploads/"
 MEDIA_URL = '/media/'
